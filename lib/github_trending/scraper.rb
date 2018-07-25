22e914a1a9a996162b9fb3cbe9b223c4fdeee45e
iwannatto <19819405+iwannatto@users.noreply.github.com>
2018-07-25 20:14:16 +0900

    Update scraper.rb

--- lib/github_trending/scraper.rb
+++ lib/github_trending/scraper.rb
@@ -36,7 +36,7 @@ module Github
         page = @agent.get(generate_url_for_get(language, since))
 
         # modify
-        page.search('.repo-list') do |content|
+        page.search('li') do |content|
           project = Project.new
           
           project.name = content.at('div h3 a')['href'][0..1000]
